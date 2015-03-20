1c59c8d50d8a172863134fa5b5790915aba09f66
Diogo Neves <diogo.neves@gmail.com>
2015-03-20 19:38:39 +0000

    More use cases

--- utils.py
+++ utils.py
@@ -36,7 +36,16 @@ def decorator(decorator_func):
 
 def safe_try(exception_types, default_value=None):
     """Wrap try except around a function and return an optional default_value
-    if an exception of exception_types is raised."""
+    if an exception of exception_types is raised.
+
+    E.g.:
+        @safe_try(KeyError, default_value='mistake')
+        def bad_function(data):
+            return data['invalid_key']
+
+        print bad_function({'valid_key': 'success'})
+        # Out: 'mistake'
+    """
     @decorator
     def try_decorator(fn, *args, **kwargs):
         try:
@@ -46,9 +55,20 @@ def safe_try(exception_types, default_value=None):
     return try_decorator
 
 
-def wrap_exceptions(exception_types, new_exception_type):
-    """Wrap try except around a function and return an optional default_value
-    if an exception of exception_types is raised."""
+def wrap_exception(exception_types, new_exception_type):
+    """Wrap any exceptions of type exception_types into new_exception_type.
+
+    E.g.:
+        class MyException(Exception):
+            pass
+
+        @wrap_exception((KeyError, ValueError), MyException)
+        def raise_it(data):
+            return data['invalid_key']
+
+        raise_it({'valid_key': 1})
+        # Will raise MyException instead of KeyError.
+    """
     @decorator
     def try_decorator(fn, *args, **kwargs):
         try:
